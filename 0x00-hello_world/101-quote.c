14f9feaaafa9cd91a948fbc8833d300404889c35
zlaouabd <zakils@yahoo.fr>
2023-07-13 17:16:57 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * Description: main - Entry point.
+ * Return: always 0 "success".
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
+}
+
