91ff729129c0c1e62886595c0297b2b2f4431d11
zlaouabd <zakils@yahoo.fr>
2023-07-31 03:56:41 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+char *_memset(char *s, char b, unsigned int n) {
+// Your implementation of _memset goes here.
+// It should fill the first 'n' bytes of the memory pointed by 's' with the value 'b'.
+// Return the pointer to the memory area 's'.
+
+return s;
+}
